c3246a5c8af43c9bfa2549fefec25eea274eb60c
Abhinav Singh <mailsforabhinav@gmail.com>
2014-03-05 05:43:30 +0530

    move to tornado SSLIOStream and SSL enabled TCPServer

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-import ssl
-import socket
+import signal
 import logging
-import threading
+from tornado.ioloop import IOLoop
+from tornado.tcpserver import TCPServer
 from Crypto.Hash import MD5
+
 import constants
 
 logging.basicConfig(level=logging.DEBUG)
@@ -14,7 +15,10 @@ clients = dict()
 # buffered messages for offline pubkey
 buffers = dict()
 
-class Handler(threading.Thread):
+def handle_signal(sig, frame):
+    IOLoop.instance().add_callback(IOLoop.instance().stop)
+
+class Handler(object):
     '''client connection handler.'''
 
     def __init__(self, conn, addr):
@@ -22,107 +26,82 @@ class Handler(threading.Thread):
         self.conn = conn
         self.addr = addr
         
-        self.client = ssl.wrap_socket(self.conn, server_side=True, certfile=constants.SERVER_CRT_PATH, keyfile=constants.SERVER_KEY_PATH)
-        
+        self.opcode = None
         self.buffer = ''
         self.pubkey = None
         self.md5 = None
     
     def run(self):
-        try:
-            data = self.client.read()
-            
-            while data:
-                #logger.info('rcvd %s from client %s' % (data, self.addr))
-                self.buffer += data
-                messages = self.buffer.split(constants.CRLF)
-                
-                # extract op
-                opcode = None
-                if len(messages) > 1:
-                    opcode = int(messages[0])
-                
-                # check if we have sufficient data for current op
-                if opcode == constants.OP_PUBKEY and len(messages) >= 3:
-                    self.handle_op_pubkey(messages[1])
-                    messages = messages[3:]
-                elif opcode == constants.OP_MESSAGE and len(messages) >= 5:
-                    message = messages[1:4]
-                    self.handle_op_message(*message)
-                    messages = messages[5:]
-                
-                self.buffer = constants.CRLF.join(messages)
-                data = self.client.read()
+        self.read_line()
+    
+    def read_line(self):
+        self.conn.read_until(constants.CRLF, self.handle_line)
+    
+    def handle_line(self, data):
+        #logger.debug('rcvd %s from client %s' % (data, self.addr))
         
-        except KeyboardInterrupt:
-            pass
+        self.buffer += data
+        messages = self.buffer.split(constants.CRLF)
         
-        except Exception, e:
-            logger.exception(e)
+        if not self.opcode and len(messages) > 1:
+            self.opcode = int(messages[0])
         
-        finally:
-            try:
-                self.client.shutdown(socket.SHUT_RDWR)
-                self.client.close()
-            except socket.error:
-                pass
+        if self.opcode == constants.OP_PUBKEY and len(messages) == 3:
+            self.handle_op_pubkey(messages[1])
+            self.buffer = ''
+            self.opcode = None
+        elif self.opcode == constants.OP_MESSAGE and len(messages) == 5:
+            message = messages[1:4]
+            self.handle_op_message(*message)
+            self.buffer = ''
+            self.opcode = None
+        
+        self.read_line()
     
     def handle_op_pubkey(self, pubkey):
         self.pubkey = pubkey
         self.md5 = MD5.new(pubkey).hexdigest()
-        clients[self.md5] = self.client
+        clients[self.md5] = self.conn
         
         if self.md5 in buffers:
             messages = buffers[self.md5]
             del buffers[self.md5]
+            logger.debug('%s offline messages found for %s' % (len(messages), self.md5))
             for message in messages:
-                self.write(self.client, *message)
+                self.write(self.conn, *message)
     
     def handle_op_message(self, pubkey, enc, sig):
         message = (constants.OP_MESSAGE, self.pubkey, enc, sig,)
         md5 = MD5.new(pubkey).hexdigest()
         
         if md5 in clients:
-            client = clients[md5]
-            self.write(client, *message)
+            conn = clients[md5]
+            self.write(conn, *message)
         else:
             if md5 not in buffers:
                 buffers[md5] = list()
             buffers[md5].append(message,)
+            logger.debug('%s offline, buffered message' % md5)
     
     @staticmethod
-    def write(client, *messages):
+    def write(conn, *messages):
         for message in messages:
-            client.write('%s%s' % (message, constants.CRLF))
+            conn.write('%s%s' % (message, constants.CRLF))
 
-class Server(object):
+class Server(TCPServer):
     '''ssl server implementation.'''
 
-    def __init__(self, port=10001):
-        self.port = port
-        self.sock = socket.socket()
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    def __init__(self):
+        super(Server, self).__init__(ssl_options={'certfile':constants.SERVER_CRT_PATH, 'keyfile':constants.SERVER_KEY_PATH})
 
-    def run(self):
-        self.sock.bind(('127.0.0.1', self.port))
-        self.sock.listen(10)
-        logging.info('listening on %s' % self.port)
-
-        try:
-            while True:
-                conn, addr = self.sock.accept()
-                handler = Handler(conn, addr)
-                handler.daemon = True
-                handler.start()
-                logger.info('conn %s from %s delegated to handler %s' % (conn, addr, handler))
-        except KeyboardInterrupt:
-            pass
-        except Exception, e:
-            logger.exception(e)
-        finally:
-            logger.info('shutting down server')
-            self.sock.close()
+    def handle_stream(self, conn, addr):
+        handler = Handler(conn, addr)
+        handler.run()
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, handle_signal)
+    signal.signal(signal.SIGTERM, handle_signal)
     s = Server()
-    s.run()
+    s.listen(10001)
+    IOLoop.instance().start()
+    IOLoop.instance().close()
